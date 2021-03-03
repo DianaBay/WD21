9a63b0b28a78a24c0dcb396fa97b7fb7c621065a
DianaBay <47646803+DianaBay@users.noreply.github.com>
2021-03-04 01:53:58 +0600

    Add files via upload
    Lab 5

---
+++ WD_Lab5/src/app/categories.ts
@@ -0,0 +1,18 @@
+export const categories = [
+  {
+    id: 1,
+    name: 'Jackets'
+  },
+  {
+    id: 2,
+    name: 'Trousers'
+  },
+  {
+    id: 3,
+    name: 'Suits'
+  },
+  {
+    id: 4,
+    name: 'Shoes'
+  }
+];
---
+++ WD_Lab5/src/app/products.ts
@@ -0,0 +1,212 @@
+export const products = [
+  {
+    id: 1,
+    category: 'Jackets',
+    image: 'https://static.zara.net/photos///2021/V/0/2/p/4387/474/707/2/w/750/4387474707_2_1_1.jpg?ts=1614096829462',
+    name: 'Check Bomber Jacket',
+    price: 62.00,
+    description: 'JACKET WITH A HIGH NECK AND LONG SLEEVES. FEATURING FLAP POCKETS AT THE HIP, RIBBED TRIMS AND A ZIP-UP FRONT.',
+    rating: '4.5 stars',
+    link: 'https://www.zara.com/kz/en/check-bomber-jacket-p04387474.html?v1=90190600&v2=1717639'
+  },
+  {
+    id: 2,
+    category: 'Jackets',
+    image: 'https://static.zara.net/photos///2021/V/0/2/p/8574/409/300/2/w/1280/8574409300_2_1_1.jpg?ts=1610996698018',
+    name: 'RUBBERISED COACH JACKET',
+    price: 47.99,
+    description: 'COLLARED JACKET WITH A LIGHTLY PADDED INTERIOR. LONG SLEEVES WITH BUTTONED CUFFS. PATCH POCKETS ON THE CHEST AND SIDE POCKETS AT THE HIP. FRONT SNAP-BUTTON FASTENING.',
+    rating: '5 stars',
+    link: 'https://www.zara.com/kz/en/rubberised-coach-jacket-p08574409.html?v1=85788263&v2=1717639'
+  },
+  {
+    id: 3,
+    category: 'Jackets',
+    image: 'https://static.zara.net/photos///2021/V/0/2/p/8281/425/806/2/w/1280/8281425806_2_1_1.jpg?ts=1611737176710',
+    name: 'BASIC BOMBER JACKET',
+    price: 61.90,
+    description: 'JACKET FEATURING A HIGH COLLAR, LONG SLEEVES WITH ELASTIC CUFFS, WELT POCKETS AT THE HIP, INSIDE POCKET DETAIL, MATCHING RIBBED HEM AND ZIP-UP FRONT.',
+    rating: '5 stars',
+    link: 'https://www.zara.com/kz/en/basic-bomber-jacket-p08281425.html?v1=89119927&v2=1717639'
+  },
+  {
+    id: 4,
+    category: 'Jackets',
+    image: 'https://static.zara.net/photos///2021/V/0/2/p/8281/407/806/2/w/1280/8281407806_2_1_1.jpg?ts=1608713527747',
+    name: 'HOODED TECHNICAL JACKET',
+    price: 15.07,
+    description: 'JACKET FEATURING AN ADJUSTABLE HOOD AND LONG SLEEVES WITH ELASTIC CUFFS. HIP PATCH POCKETS WITH FLAPS, INSIDE POCKET DETAIL, ADJUSTABLE HEM WITH ELASTIC TRIM ON THE SIDES AND A ZIP-UP FRONT HIDDEN BY A SNAP-BUTTON PLACKET.',
+    rating: '4.7 stars',
+    link: 'https://www.zara.com/kz/en/hooded-technical-jacket-p08281407.html?v1=85659211&v2=1717639'
+  },
+  {
+    id: 5,
+    category: 'Jackets',
+    image: 'https://static.zara.net/photos///2021/V/0/2/p/3791/400/400/2/w/1280/3791400400_2_4_1.jpg?ts=1611574802209',
+    name: 'REVERSIBLE FAUX SHEARLING JACKET',
+    price: 71.00,
+    description: 'CONTRAST REVERSIBLE JACKET. STAND-UP COLLAR AND LONG SLEEVES WITH TRIMMED BUTTONED CUFFS. WELT POCKETS AT THE HIP. BUTTON-UP FRONT.',
+    rating: '4.5 stars',
+    link: 'https://www.zara.com/kz/en/reversible-faux-shearling-jacket-p03791400.html?v1=95124468&v2=1717639'
+  },
+  {
+    id: 6,
+    category: 'Trousers',
+    image: 'https://static.zara.net/photos///2021/V/0/2/p/4158/773/710/2/w/1280/4158773710_2_1_1.jpg?ts=1612195359972',
+    name: 'COTTON CHINO TROUSERS',
+    price: 62.00,
+    description: 'TROUSERS MADE OF COMPACT COTTON FABRIC. WAIST WITH PLEATED DETAIL ON THE FRONT, FRONT POCKETS, REAR WELT POCKETS WITH BUTTON AND ZIP FLY AND TOP BUTTON FASTENING.',
+    rating: '4.7 stars',
+    link: 'https://www.zara.com/kz/en/cotton-chino-trousers-p04158773.html?v1=88907921&v2=1720241'
+  },
+  {
+    id: 7,
+    category: 'Trousers',
+    image: 'https://static.zara.net/photos///2021/V/0/2/p/0706/140/401/2/w/1280/0706140401_1_1_1.jpg?ts=1614273563155',
+    name: 'COMFORT FIT JOGGER WAIST TROUSERS',
+    price: 73.00,
+    description: 'TROUSERS MADE OF COMFORTABLE STRETCH TEXTURED FABRIC. ADJUSTABLE ELASTIC DRAWSTRING WAIST, IN-SEAM SIDE HIP POCKETS, BACK WELT POCKET DETAILS AND TURN-UP HEMS.',
+    rating: '5 stars',
+    link: 'https://www.zara.com/kz/en/comfort-fit-jogger-waist-trousers-p00706140.html?v1=93619974&v2=1720241'
+  },
+  {
+    id: 8,
+    category: 'Trousers',
+    image: 'https://static.zara.net/photos///2021/V/0/2/p/1620/403/505/2/w/1280/1620403505_2_1_1.jpg?ts=1614334926072',
+    name: 'CROPPED CARGO TROUSERS',
+    price: 70.00,
+    description: 'CROPPED TROUSERS WITH AN ELASTIC WAIST AND ADJUSTABLE INTERIOR. FEATURING FRONT POCKETS, BACK POCKETS, PATCH POCKETS WITH FLAPS ON THE LEGS, CUFFED ELASTIC HEMS AND ZIP FLY AND TOP BUTTON FASTENING.',
+    rating: '4.8 stars',
+    link: 'https://www.zara.com/kz/en/cropped-cargo-trousers-p01620403.html?v1=100625712&v2=1720241'
+  },
+  {
+    id: 9,
+    category: 'Trousers',
+    image: 'https://static.zara.net/photos///2021/V/0/2/p/4087/422/711/2/w/1280/4087422711_2_1_1.jpg?ts=1606473866285',
+    name: 'BASIC JOGGING TROUSERS',
+    price: 35.70,
+    description: 'TROUSERS WITH AN ELASTIC DRAWSTRING WAISTBAND. FEATURING SIDE WELT POCKETS, A REAR PATCH POCKET AND CUFFED ELASTIC HEMS.',
+    rating: '4.8 stars',
+    link: 'https://www.zara.com/kz/en/basic-jogging-trousers-p04087422.html?v1=99741753&v2=1720241'
+  },
+  {
+    id: 10,
+    category: 'Trousers',
+    image: 'https://static.zara.net/photos///2021/V/0/2/p/1620/403/505/2/w/1280/1620403505_2_1_1.jpg?ts=1614598387345',
+    name: 'CROPPED CARGO TROUSERS',
+    price: 29.90,
+    description: 'CROPPED TROUSERS WITH AN ELASTIC WAIST AND ADJUSTABLE INTERIOR. FEATURING FRONT POCKETS, BACK POCKETS, PATCH POCKETS WITH FLAPS ON THE LEGS, CUFFED ELASTIC HEMS AND ZIP FLY AND TOP BUTTON FASTENING.',
+    rating: '4.9 stars',
+    link: 'https://www.zara.com/kz/en/cropped-cargo-trousers-p01620403.html?v1=86571746&v2=1720241'
+  },
+  {
+    id: 11,
+    category: 'Suits',
+    image: 'https://static.zara.net/photos///2021/V/T/2/p/0156/441/800/2/w/1280/0156441800_1_1_1.jpg?ts=1611660462004',
+    name: 'SLIM FIT SUIT',
+    price: 169.95,
+    description: 'BLAZER WITH A POINTED LAPEL COLLAR. LONG SLEEVES WITH BUTTON DETAIL ON THE CUFFS. WELT POCKETS AT THE CHEST AND FLAP POCKETS AT THE HIP. INSIDE POCKET DETAIL. FRONT BUTTON FASTENING.\n' +
+      '\n' +
+      'SLIM FIT TROUSERS FEATURING FRONT POCKETS, REAR JETTED POCKETS AND ZIP FLY AND TOP BUTTON FASTENING.',
+    rating: '4.5 stars',
+    link: 'https://www.zara.com/kz/en/slim-fit-suit-pT0156441800.html?v1=88243293&v2=1717689'
+  },
+  {
+    id: 12,
+    category: 'Suits',
+    image: 'https://static.zara.net/photos///2021/V/T/2/p/0640/353/401/2/w/1280/0640353401_1_1_1.jpg?ts=1611661123091',
+    name: 'Sorel - Women\'s Explorer Joan Waterproof Insulated Winter Boot',
+    price: 134.95,
+    description: 'BLAZER WITH A PEAK LAPEL COLLAR, LONG SLEEVES AND DECORATIVE BUTTONS ON THE CUFFS. FEATURING CHEST AND HIP WELT POCKETS, INSIDE POCKET, TWO BACK VENTS AND FRONT BUTTON FASTENING.\n' +
+      '\n' +
+      'SLIM FIT TROUSERS WITH FRONT POCKETS AND REAR WELT POCKETS. FEATURING TURN-UP HEMS AND ZIP FLY AND TOP BUTTON FASTENING.',
+    rating: '5.0 stars',
+    link: 'https://www.zara.com/kz/en/textured-suit-pT0640353401.html?v1=89722106&v2=1717689'
+  },
+  {
+    id: 13,
+    category: 'Suits',
+    image: 'https://static.zara.net/photos///2021/V/T/2/p/0070/647/403/2/w/1280/0070647403_1_1_1.jpg?ts=1612349737384',
+    name: 'COMFORT TEXTURED SUIT',
+    price: 159.95,
+    description: 'BLAZER WITH A LAPEL COLLAR, LONG SLEEVES AND BUTTONED CUFFS. FEATURING WELT POCKETS ON THE CHEST AND HIPS, TWO BACK VENTS AND FRONT BUTTON FASTENING.\n' +
+      '\n' +
+      'SLIM FIT TROUSERS WITH FRONT POCKETS AND REAR WELT POCKETS. FEATURING TURN-UP HEMS AND ZIP FLY AND TOP BUTTON FASTENING.',
+    rating: '4.5 stars',
+    link: 'https://www.zara.com/kz/en/comfort-textured-suit-pT0070647403.html?v1=97858340&v2=1717689'
+  },
+  {
+    id: 14,
+    category: 'Suits',
+    image: 'https://static.zara.net/photos///2021/V/T/2/p/0427/099/700/2/w/1280/0427099700_1_1_1.jpg?ts=1614092507594',
+    name: 'CHECK SUIT',
+    price: 81.95,
+    description: 'BLAZER WITH A NOTCHED LAPEL COLLAR. LONG SLEEVES WITH BUTTONED CUFFS. HIP FLAP POCKETS AND AN INSIDE POCKET. CENTRAL BACK VENT AT THE HEM. FRONT BUTTON FASTENING.\n' +
+      '\n' +
+      'SLIM FIT TROUSERS FEATURING FRONT POCKETS AND BUTTONED WELT POCKETS AT THE BACK. FRONT ZIP AND TOP BUTTON FASTENING.',
+    rating: '4.7 stars',
+    link: 'https://www.zara.com/kz/en/check-suit-pT0427099700.html?v1=100217829&v2=1717689'
+  },
+  {
+    id: 15,
+    category: 'Suits',
+    image: 'https://static.zara.net/photos///2021/V/T/2/p/0416/153/707/2/w/1280/0416153707_1_1_1.jpg?ts=1611760665366',
+    name: 'TEXTURED SUIT',
+    price: 104.45,
+    description: 'LIGHTWEIGHT BLAZER WITH A PEAK LAPEL COLLAR. LONG SLEEVES WITH BUTTON DETAIL AT THE CUFFS. HIP PATCH POCKETS. BACK VENT. FRONT DOUBLE-BREASTED BUTTON FASTENING.\n' +
+      '\n' +
+      'TROUSERS WITH FRONT PLEAT DETAIL AT THE WAIST. FRONT POCKETS AND REAR WELT POCKET DETAIL. FRONT ZIP FLY AND INVISIBLE METAL HOOK FASTENING.',
+    rating: '4.8 stars',
+    link: 'https://www.zara.com/kz/en/textured-suit-pT0416153707.html?v1=95125944&v2=1717689'
+  },
+  {
+    id: 16,
+    category: 'Shoes',
+    image: 'https://static.zara.net/photos///2020/I/1/2/p/2229/620/100/2/w/1280/2229620100_2_1_1.jpg?ts=1603268192216',
+    name: 'BROWN MULTI-PIECE TRAINERS',
+    price: 34.95,
+    description: 'MULTI-PIECE TRAINERS WITH LACES. DARK BROWN IN A COMBINATION OF DIFFERENT SHADES ON THE UPPER. SIX-EYELET FACING. SOLE ALSO FEATURES A COMBINATION OF COLOURS. TRACK SOLE.',
+    rating: '4.5 stars',
+    link: 'https://www.zara.com/kz/en/brown-multi-piece-trainers-p12229620.html?v1=86069890&v2=1720413'
+  },
+  {
+    id: 17,
+    category: 'Shoes',
+    image: 'https://static.zara.net/photos///2020/I/1/2/p/2318/621/203/2/w/1280/2318621203_2_1_1.jpg?ts=1604057854971',
+    name: 'MULTI-PIECE SNEAKERS',
+    price: 39.50,
+    description: 'LACE-UP SNEAKERS. AVAILABLE IN SEVERAL COLOURS. CONTRAST PIECES AND MATERIALS ON THE UPPER. SEVEN-EYELET FACING AND BACK PULL TAB. CHUNKY SOLE.',
+    rating: '4.8 stars',
+    link: 'https://www.zara.com/kz/en/multi-piece-sneakers-p12318621.html?v1=83737556&v2=1720413'
+  },
+  {
+    id: 18,
+    category: 'Shoes',
+    image: 'https://static.zara.net/photos///2020/I/1/2/p/2402/620/040/2/w/1280/2402620040_2_1_1.jpg?ts=1603707869934',
+    name: 'SMART BLACK SHOES',
+    price: 40.17,
+    description: 'SMART BLACK DERBY SHOES. SIX-EYELET FACING. DECORATIVE SEAMS AND EMBOSSING ON THE BACK OF THE UPPER.',
+    rating: '5.0 stars',
+    link: 'https://www.zara.com/kz/en/smart-black-shoes-p12402620.html?v1=86069864&v2=1720413'
+  },
+  {
+    id: 19,
+    category: 'Shoes',
+    image: 'https://static.zara.net/photos///2021/V/1/2/p/2412/720/002/2/w/1280/2412720002_6_2_1.jpg?ts=1611314604278',
+    name: 'TRACK SOLE SHOES',
+    price: 52.90,
+    description: 'ECRU LACE-UP SHOES. FIVE-EYELET FACING. CHUNKY TRACK SOLE WITH CONTRAST BLACK WELT DETAIL. ROUND TOE. STREET LOOK - SPORTY',
+    rating: '4.8 stars',
+    link: 'https://www.zara.com/kz/en/track-sole-shoes-p12412720.html?v1=84962858&v2=1720413&page=3'
+  },
+  {
+    id: 20,
+    category: 'Shoes',
+    image: 'https://static.zara.net/photos///2021/V/1/2/p/2102/720/107/2/w/1280/2102720107_2_1_1.jpg?ts=1606400712309',
+    name: 'SOCK-STYLE HIGH-TOP SNEAKERS',
+    price: '86.96',
+    description: 'BLACK AND GREY HIGH-TOP TRAINERS MADE OF TECHNICAL FABRIC. WRAPAROUND UPPER IN A SINGLE PIECE WITH A SOCK-STYLE DESIGN AND THREE-EYELET FACING. FRONT AND BACK PULL TABS. CONTRAST BLACK AND WHITE CHUNKY TRACK SOLE.',
+    rating: '5.0 stars',
+    link: 'https://www.zara.com/kz/en/sock-style-high-top-sneakers-p12102720.html?v1=78496073&v2=1720413'
+  }
+];
