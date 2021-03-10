fa5937ac9b2ae3c91aa7ab6d2ef1381ad1bfd9f3
DianaBay <dianabayandina@gmail.com>
2021-03-10 14:52:24 +0600

    Lab 6

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { AlbumDetailComponent } from './album-detail/album-detail.component';
+import { AlbumPhotosComponent } from './album-photos/album-photos.component';
+import { AlbumsComponent } from './albums/albums.component';
+import { HomeComponent } from './home/home.component';
+
+
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'albums', component: AlbumsComponent},
+  {path: 'albums/:id', component: AlbumDetailComponent},
+  {path: 'albums/:id/photo', component: AlbumPhotosComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { AlbumsComponent } from './albums/albums.component';
+import { AlbumDetailComponent } from './album-detail/album-detail.component';
+import { AlbumPhotosComponent } from './album-photos/album-photos.component';
+import {FormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    HomeComponent,
+    AlbumsComponent,
+    AlbumDetailComponent,
+    AlbumPhotosComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
